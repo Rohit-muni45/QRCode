b50a8803b6352fbe3963429ab9df07dbc58c799a
Muni Neelam <nagamunindraneelam@gmail.com>
2025-08-06 16:32:27 +0530

    ChnagedUrl

--- src/Components/QRCode.jsx
+++ src/Components/QRCode.jsx
@@ -5,7 +5,7 @@ function QRCode() {
     <div className='px-10 py-4'>
       <h2 className='text-2xl font-bold mb-4'>QR Code</h2>  
       <QRCodeSVG
-        value="http://localhost:5173/profile" />
+        value="https://qr-code-roan-two.vercel.app/profile" />
     </div>
   )
 }
