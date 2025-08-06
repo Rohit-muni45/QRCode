cf9545d143a78893fc48248a6fcabf68e1784189
Muni Neelam <nagamunindraneelam@gmail.com>
2025-08-06 16:50:47 +0530

    Changes in QR value

--- src/Components/QRCode.jsx
+++ src/Components/QRCode.jsx
@@ -5,7 +5,8 @@ function QRCode() {
     <div className='px-10 py-4'>
       <h2 className='text-2xl font-bold mb-4'>QR Code</h2>  
       <QRCodeSVG
-        value="https://qr-code-roan-two.vercel.app/profile" />
+        //value="https://qr-code-roan-two.vercel.app/profile" 
+        value="www.youtube.com"/>
     </div>
   )
 }
